d9f5997943239ef1b2d3faaed5faa2e2ff5a39a0
MatthiasBenaets <matthias.benaets@gmail.com>
2025-08-11 15:02:57 +0200

    load applications on demand on nodes

---
+++ ansible/slurm/playbooks/example/lmod/opt/modulefiles/R/4.4.0.lua
@@ -0,0 +1,8 @@
+whatis("Description: R programming language version 4.4.0")
+
+-- Set the R_HOME and R_LIBS_USER environment variables
+setenv("R_HOME", "/opt/apps/R/4.4.0/lib/R")
+setenv("R_LIBS_USER", "/opt/apps/R/4.4.0/library")
+
+-- Add the R binary to the user's PATH
+prepend_path("PATH", "/opt/apps/R/4.4.0/bin")
